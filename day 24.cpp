cad1e0672c5efaebb6baf223ed65831d84f673ce
sakshikhule <102469697+sakshikhule@users.noreply.github.com>
2022-12-12 16:55:48 +0530

    Add files via upload

---
+++ day 24.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int i, j, k, rows;
+ cout<<"Enter number of rows: ";
+ cin>>rows;
+ for(i=1; i<=rows; i++)
+ {
+ for(j=i; j<rows; j++)
+ cout<<" ";
+ for(k=1; k<=(2*i-1); k++)
+ cout<<"*";
+ cout<<"\n";
+ }
+ return 0;
+}
+
